40728d827a232144493d39965ca67b6a13c175ec
Rose Lu <roselu@stud.ntnu.no>
2020-02-29 13:53:53 +0100

    Oppdatert doxygen-dokumentasjon

--- source/queue.h
+++ source/queue.h
@@ -1,15 +1,57 @@
+/**
+ * @brief The elevator's queue system
+ */
+
 #include "hardware.h"
 #include <stdlib.h>
+#include <stdio.h>
 
+
+
+
+/**
+ * @brief The queue system's matrix with floors and order types inlcuded
+ * @param queue_matrix The queue system's matrix
+ */
 int queue_matrix[4][3];
 
-//void order_queue_matrix (int i, int j);
+
+/**
+ * @brief Empty all the order(s) from the queue matrix
+ * @param void No input argument(s)
+ */ 
+void empty_all_orders(void);
 
 
-void empty_all_orders();
-
+/**
+ * @brief Add order to the queue matrix
+ * @param floor Inquired floor
+ * @param order Type of order
+ */
 void add_order(int floor, HardwareOrder order);
+
+
+/**
+ * @brief Deletes order from the queue matrix
+ * @param floor Inquired floor
+ * @param order Type of order
+ */
 void delete_order(int floor, HardwareOrder order);
 
+
+/**
+ * @brief Checks if there's any order above the elevator's current floor
+ * @param current_floor The elevator's current floor
+ * @param motor_direction The elevator's motor direction
+ * @return 1 if it's a order; 0 otherwise
+ */
 int queue_order_above(int current_floor, HardwareMovement motor_direction);
+
+
+/**
+ * @brief Checks if there's any order below the elevator's current floor
+ * @param current_floor The elevator's current floor
+ * @param motor_direction The elevator's motor direction
+ * @return 1 if it's a order; 0 otherwise
+ */
 int queue_order_below(int current_floor, HardwareMovement motor_direction);
