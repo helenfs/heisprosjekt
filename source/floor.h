46e9599edee57cd5badd2382121564e704355947
Rose Lu <roselu@stud.ntnu.no>
2020-02-29 13:24:39 +0100

    Oppdatert doxygen-dokumentasjon

--- source/floor.h
+++ source/floor.h
@@ -3,7 +3,30 @@
 #include <stdlib.h>
 #include <stdio.h>
 
+/**
+ * @brief Checks the elevator's position and if it's any orders above or below the elevator's current floor
+ * */
 
-int floor_indicator();
+
+/**
+ * @brief Checks the elevator's position in real time
+ * @param void No input argument(s)
+ * @return The number of the floor, and -1 if between floors
+ * */
+int floor_indicator(void);
+
+
+/**
+ * @brief Checks if there's any order(s) above the elevator's current floor
+ * @param current_floor The elevator's position
+ * @return The number of floor with order(s); -1 if no order(s)
+ * */
 int get_ordered_above(int current_floor);
+
+
+/**
+ * @brief Checks if there's any order(s) below the elevator's current floor
+ * @param current_floor The elevator's position
+ * @return The number of floor with order(s); 4 if no order(s)
+ * */
 int get_ordered_below(int current_floor);
