d58b89320981f89f8c91e8079933c0f1d2ffbd29
Rose Lu <roselu@stud.ntnu.no>
2020-02-29 13:10:14 +0100

    Fjernet kommentarer og flyttet #include til header-filen

--- source/timer.c
+++ source/timer.c
@@ -6,13 +6,13 @@ int waiting_time=3;
 time_t our_timer;
 
 
-void set_timer(){
+void set_timer() {
 	our_timer=time(NULL);
 }
 
 
-int check_timer(){
-	if(our_timer + waiting_time > time(NULL)) { //our_timer.time + waiting_time > time(NULL)
+int check_timer() {
+	if (our_timer + waiting_time > time(NULL)) {
 		return 1;
 	}
     return 0;
