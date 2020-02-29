813f6a864b36ad5b22b047d252b3f24762260c6b
Rose Lu <roselu@stud.ntnu.no>
2020-02-29 13:12:05 +0100

    Skrevet ferdig Doxygen for timer.h

--- source/timer.h
+++ source/timer.h
@@ -1,5 +1,21 @@
 #include <time.h>
 #include "hardware.h"
 
+/**
+ * @brief Timer to start and stop the waiting time
+ * */
+
+
+/**
+ * @brief Starts the timer
+ * @param void No input argument(s)
+ * */
 void set_timer();
+
+
+/**
+ * @brief Checks if the timer have counted x seconds from the start time
+ * @param void No input argument(s)
+ * @return 1 haven't counted x seconds and 0 if the timer have counted x seconds
+ * */
 int check_timer();
